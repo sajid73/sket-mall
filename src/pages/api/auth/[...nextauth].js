946ce474a308f6822a530dbd192350fdba46b282
sajid73 <smsajid7300@gmail.com>
2021-05-29 12:39:14 +0600

    google key updated

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,8 +5,8 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientId: process.env.GOOGLE_ID,  //650135736804-ee4t6dvnt7n6t6q5q6s3gs7rs3c8boid.apps.googleusercontent.com
+      clientSecret: process.env.GOOGLE_SECRET   //TeZIIM1mFIle5ZplmOMrAnw9
     }),
     // ...add more providers here
   ],
