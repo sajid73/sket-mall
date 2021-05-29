bc6bf54db48fff37eb81c007282deaf350fc39d8
sajid73 <smsajid7300@gmail.com>
2021-05-29 12:20:33 +0600

    Day 2 contribuition from sajid

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8PzcZBiF8Sx1wQXSxQHdRz9Jbs4bZuWM",
+  authDomain: "sket-mall.firebaseapp.com",
+  projectId: "sket-mall",
+  storageBucket: "sket-mall.appspot.com",
+  messagingSenderId: "650135736804",
+  appId: "1:650135736804:web:e183d6dfd7e8a54a34f7f1",
+};
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -2,48 +2,67 @@ import Image from "next/image";
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid";
 import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
+
 const MAX_RATING = 5;
-const MIN_RATING =1;
+const MIN_RATING = 1;
 function Product({ id, title, price, description, category, image }) {
+  const dispatch = useDispatch();
 
-    const [rating] = useState(
-        Math.floor(Math.random()* (MAX_RATING-MIN_RATING+1))+MIN_RATING
-    );
-    const [hasPrime] = useState(Math.random()<0.5)
-    return (
-        <divn className ='relative flex flex-col m-5 bg-white z-30 p-10'>
-            <p className="absolute top-2 right-2 text-xs italic text-gray-400 " >{category}</p>
+  const [rating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  );
+  const [hasPrime] = useState(Math.random() < 0.5);
 
-            <Image src={image} height={200} width={200} objectFit="contain" />
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      description,
+      category,
+      image,
+      hasPrime
+    };
 
+    dispatch(addToBasket(product));
+  };
 
-            <h4 className ='my-3' >{title}</h4>
+  return (
+    <divn className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400 ">
+        {category}
+      </p>
 
-            <div className = 'flex'>
-                {Array(rating)
-                .fill()
-                .map((_, i) =>(
-                    <StarIcon className='h-5 text-yellow-500' ></StarIcon>
-                 ) )}
-            </div>
-                <p className="text-xs mt-2 my-2 line-clamp-2" >{description}</p>
+      <Image src={image} height={200} width={200} objectFit="contain" />
 
-                <div className ='mb-5' >
-                    <Currency quantity ={price} currency = 'GBP'></Currency>
-                </div>
+      <h4 className="my-3">{title}</h4>
 
-                {
-                    hasPrime && (
-                        <div className ='flex items-center space-x-2 -mt-2' >
-                            <img className ='w-12' src="https://links.papareact.com/fdw" alt="" />
-                            <p className='text-xs text-gray-500 '>FERR Next-day Delivary</p>
-                        </div>
-                    )
-                    
-                }
-                <button className ="mt-auto button " >Add to Busket</button>
-        </divn>
-    )
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon className="h-5 text-yellow-500"></StarIcon>
+          ))}
+      </div>
+      <p className="text-xs mt-2 my-2 line-clamp-2">{description}</p>
+
+      <div className="mb-5">
+        <Currency quantity={price} currency="GBP"></Currency>
+      </div>
+
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-2">
+          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+          <p className="text-xs text-gray-500 ">FERR Next-day Delivary</p>
+        </div>
+      )}
+      <button onClick={addItemToBasket} className="mt-auto button">
+        Add to Busket
+      </button>
+    </divn>
+  );
 }
 
-export default Product
+export default Product;
